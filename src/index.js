1533a8039dadb470d25c81db9349a74c297e6fca
KurtReyn <kurtreyn@yahoo.com>
2022-03-24 11:31:23 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
+/* eslint-disable default-case */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+
+// STORE -> GLOBALIZED STATE
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +44,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
